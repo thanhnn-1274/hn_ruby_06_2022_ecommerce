9d0eef4204bbbabb8da4299cf85d60e8a7069271
Nguyen Tien Dan <Dan123456dz@gmail.com>
2022-07-13 18:13:29 +0700

    User Register

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
       resources :categories
       get "/home", to: "static_pages#home"
     end
+    root "static_pages#home"
+    get "/signup", to: "users#new"
+    resources :users, only: %i(create show new)
   end
 end
