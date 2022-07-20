20604afcde2074e166894d208cb575a5ca6e3899
Nguyen Tien Dan <Dan123456dz@gmail.com>
2022-07-20 09:54:03 +0700

    Update Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   before_action :logged_in_user, only: %i(index create)
-  before_action :init_cart, only: %i(index create)
-  before_action :load_product, only: %i(create)
+  before_action :init_cart, only: %i(index create update destroy)
+  before_action :load_product, only: %i(create update destroy)
   before_action :load_products, only: :index
 
   def index; end
@@ -13,6 +13,27 @@ class CartsController < ApplicationController
       flash[:danger] = t ".danger_quantily"
     end
     redirect_to book_path @product.id
+  end
+
+  def update
+    if @carts.key? params[:id]
+      update_item
+    else
+      flash[:danger] = t ".fail_update"
+    end
+    redirect_to carts_path
+  end
+
+  def destroy
+    if @carts.key? params[:id]
+      @carts.delete params[:id]
+      flash[:success] = t ".success_delete"
+      user_id = session[:user_id]
+      session["cart_#{user_id}"] = @carts
+    else
+      flash[:danger] = t ".fail_delete"
+    end
+    redirect_to carts_path
   end
 
   private
@@ -39,5 +60,16 @@ class CartsController < ApplicationController
       session["cart_#{user_id}"] = @carts
     end
     flash[:success] = t ".success_add"
+  end
+
+  def update_item
+    if check_quantily @product
+      @carts[params[:id]] = @quantily
+      user_id = session[:user_id]
+      session["cart_#{user_id}"] = @carts
+      flash[:success] = t ".success_update"
+    else
+      flash[:danger] = t ".danger_quantily"
+    end
   end
 end
